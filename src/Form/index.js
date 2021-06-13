ded3f40415a04724eefaaf9c1d61e3dbf05885ee
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-06-13 22:10:36 +0200

    Form componend & style added

---
+++ src/Form/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./style.css"
+
+
+
+
+
+const Form = () => (
+    <form className="form form">
+    <fieldset className="form__fieldset">
+        <legend className="form__legend">
+            Kalkulator Walutowy
+        </legend>
+        <p className="form__legendText"> /pole oznaczone * jest wymagane </p>
+            <label className="form__labelText">
+                PLN*
+                <input className="input" type="number" min="0.01" step="any" required />
+            </label>
+            <p className="form__button">
+                <input className="form__button--input submit" type="submit" value="Przelicz!" />
+            </p>
+    </fieldset>
+
+</form>
+);
+
+export default Form;
