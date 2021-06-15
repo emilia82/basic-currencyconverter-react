6fe9ee5d41f2769759ee5038994652df8ddcb2bb
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-06-15 12:20:33 +0200

    code ordered

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Form from './Form';
-
+import Result from './Result';
 
 
 function App() {
   return (
     <div className="container">
    <Form />
-    <p className="form__result">Twój wynik Euro:    <strong className="result"></strong></p>
+    <p className="form__result">Twój wynik Euro:    
+    <strong className="result"></strong></p>
     </div>
   );
 }
