fc9676b18358f544ce40457bfa253ac9481c59e7
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-06-15 12:19:37 +0200

    const Result created

---
+++ src/Result/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./style.css" from 'Form';
+
+export const Result = () => (
+    <p className="form__result">
+    Twój wynik Euro:    
+    <strong className="result">
+        </strong></p>
+);
+
+export default Result;
