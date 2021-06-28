8301a5046f43e701443d619b6f3b178c26ad037a
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-06-28 10:49:43 +0200

    new currencies added

--- src/currency.js
+++ src/currency.js
@@ -4,6 +4,26 @@ const currencies = [
     name: "Euro",
     rate: 4.5649,
 },
+
+{
+    id: "USD",
+    name: "Dolar amerykański",
+    rate: 3.7855,
+},
+
+{
+    id: "GB",
+    name: "Funt Brytyjski",
+    rate: 5.2579,
+},
+
+{
+    id: "CHF",
+    name: "Frank Szwajcarski",
+    rate: 4.1148,
+},
+
+   
 ];
 
 export default currencies;
