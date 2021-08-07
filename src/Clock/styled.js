4602b7721047a2a424ca975c840e57055a3ec69a
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-08-07 17:13:33 +0200

    clock styled.js added

---
+++ src/Clock/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    font-size: 14px;
+    display: flex;
+    text-align: ight;
+    font-family: "Open-Sans", sans-serif;
+
+`;
