5c83d9b0679fc41a6972dfe4a7d2cc21c67d018e
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-08-07 19:50:59 +0200

    styled.js in container added

---
+++ src/Container/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"; 
+
+export const Wrapper = styled.div`
+flex-basis: 1000px;
+    margin: 15px;
+    padding: 0 20px;
+    background: ${({ theme }) => theme.color.white};
+    box-shadow: 0 0 10px ${({ theme }) => theme.color.blue};
+`;
+
+    
