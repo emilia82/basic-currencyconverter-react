9ff05295b8eedbfe35a58c3af46520007b539654
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-08-07 17:11:17 +0200

    global styles created

---
+++ src/GlobalStyle.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+import background from "./backgroundimage.jpg";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+  box-sizing: border-box;
+}
+
+*, ::after, ::before {
+  box-sizing: inherit;
+}
+
+#root {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-image: url("${background}");
+  background-position: center;
+  min-height: 115vh;
+  font-family: 'Open Sans', sans-serif;
+}
+
+`;
