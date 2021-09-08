b712c162e94596174838d3b554c0468308289ac6
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-09-08 14:45:47 +0200

    border-radius added to Wrapper

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -6,5 +6,6 @@ export const Wrapper = styled.div`
     padding: 0 20px;
     background: ${({ theme }) => theme.color.white};
     box-shadow: 0 0 10px ${({ theme }) => theme.color.blue};
+    border-radius: 10px;
 `;
 
