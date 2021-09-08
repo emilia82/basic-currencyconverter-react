ccf2f2c6750bcb6259152ae657c6fef0d4b610f9
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-09-08 13:44:10 +0200

    StyledResult added

--- src/Result/index.js
+++ src/Result/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Wrapper } from '../Clock/styled';
-
+import { StyledResult } from './styled';
 
 export const Result = ({ result }) => (
     <Wrapper>
+        <StyledResult />
         {!!result && (
             <>
-                {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;={" "}
+                {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;=&nbsp; {" "}
                 <strong>
                     {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
                 </strong>
