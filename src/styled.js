33bc6414f85a69d766a57fa8bd15a43498194076
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-09-08 16:11:23 +0200

    flex basis changed to 800px

--- src/styled.js
+++ src/styled.js
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    flex-basis: 1000px;
+    flex-basis: 800px;
     margin: 15px;
     padding: 0 20px;
     background: ${({ theme }) => theme.color.white};
     box-shadow: 0 0 10px ${({ theme }) => theme.color.blue};
+    border-radius: 10px;
 `;
